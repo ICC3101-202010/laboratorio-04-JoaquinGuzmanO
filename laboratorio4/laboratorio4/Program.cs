46112723241d8513b27fb6a00328125684d13df9
jaguzman <jaguzman1@miuandes.cl>
2020-04-17 21:11:34 -0400

    prueba

---
+++ laboratorio4/laboratorio4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace laboratorio4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
